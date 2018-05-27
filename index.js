af6935ef1cbccdba775eec0ac5ffecbd0593075f
l-kimleng <l.kimleng.surface.kh@hotmail.com>
2018-05-27 19:16:36 -0400

    First commit deploy

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ app.use(express.json());
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
+require('./startup/prod')(app);
 
 app.listen(3030, () => console.log('Server listen to port 3030...'));
