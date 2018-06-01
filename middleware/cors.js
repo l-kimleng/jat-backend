6752d7bf09a75f634848c9130084bb9f84189188
l-kimleng <l.kimleng.surface.kh@hotmail.com>
2018-06-01 12:51:19 -0400

    add allow custom header auth token

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,7 +1,7 @@
 
 module.exports = function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-auth-token, Content-Type, Accept");
     res.header("Access-Control-Expose-Headers", "x-auth-token");
     next(); 
 }
